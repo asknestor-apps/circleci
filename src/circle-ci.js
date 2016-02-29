08b211ae01d22f3874cb030dd6d26bd7dedd33df
arunthampi <arun.thampi@gmail.com>
2016-02-29 15:48:43 -0800

    Port the remaining Circle commands

--- src/circle-ci.js
+++ src/circle-ci.js
@@ -1,11 +1,12 @@
 var url = require('url');
 var util = require('util');
 var querystring = require('querystring');
+
 var circleciHost = process.env.NESTOR_CIRCLECI_HOST ? process.env.NESTOR_CIRCLECI_HOST : "circleci.com";
 var endpoint = "https://" + circleciHost + "/api/v1";
 
 var toProject = function(project) {
-  if (project.indexOf("/") === -1 && (process.env.NESTOR_GITHUB_ORG != null)) {
+  if (project.indexOf("/") === -1 && (process.env.NESTOR_GITHUB_ORG !== null)) {
     return process.env.NESTOR_GITHUB_ORG + "/" + project;
   } else {
     return project;
@@ -24,11 +25,11 @@ var formatBuildStatus = function(build) {
   return (toDisplay(build.status)) + " in build " + build.build_num + " of " + build.vcs_url + " [" + build.branch + "/" + (toSha(build.vcs_revision)) + "] " + build.committer_name + ": " + build.subject + " - " + build.why;
 };
 
-var retryBuild = function(msg, endpoint, project, build_num) {
-  msg.http(endpoint + "/project/" + project + "/" + build_num + "/retry?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
+var retryBuild = function(robot, msg, endpoint, project, build_num, done) {
+  robot.http(endpoint + "/project/" + project + "/" + build_num + "/retry?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
     "Accept": "application/json"
   }).post('{}')(handleResponse(msg, function(response) {
-    return msg.send("Retrying build " + build_num + " of " + project + " [" + response.branch + "] with build " + response.build_num);
+    msg.send("Retrying build " + build_num + " of " + project + " [" + response.branch + "] with build " + response.build_num , done);
   }));
 };
 
@@ -47,85 +48,60 @@ var getProjectsByStatus = function(robot, msg, endpoint, status, action, done) {
         projects.push(project);
       }
     }
+
     if (action === 'list') {
-      listProjectsByStatus(robot, msg, projects, status, done);
-    } else if (action === 'retry') {
-      retryProjectsByStatus(robot, msg, projects, status, done);
+      listProjectsByStatus(msg, projects, status, done);
     }
   }));
 };
 
-var retryProjectsByStatus = function(robot, msg, projects, status, done) {
-  var build_branch, i, last_build, len, project, results;
-  results = [];
-  for (i = 0, len = projects.length; i < len; i++) {
-    project = projects[i];
-    build_branch = project.branches[project.default_branch];
-    last_build = build_branch.recent_builds[0];
-    project = toProject(project.reponame);
-    results.push(retryBuild(msg, endpoint, project, last_build.build_num));
-  }
-  return results;
-};
-
-var listProjectsByStatus = function(msg, projects, status) {
-  var build_branch, i, last_build, len, message, project;
+var listProjectsByStatus = function(msg, projects, status, done) {
+  var build_branch, i, last_build, len, project;
   if (projects.length === 0) {
-    return msg.send("No projects match status " + status);
+    msg.send("No projects match status " + status, done);
   } else {
-    message = "Projects where the last build's status is " + status + ":\n";
+    var message = "Projects where the last build's status is " + status + ":\n";
     for (i = 0, len = projects.length; i < len; i++) {
       project = projects[i];
       build_branch = project.branches[project.default_branch];
       last_build = build_branch.recent_builds[0];
       message = message + ((toDisplay(last_build.outcome)) + " in build https://circleci.com/gh/" + project.username + "/" + project.reponame + "/" + last_build.build_num + " of " + project.vcs_url + " [" + project.default_branch + "]\n");
     }
-    return msg.send("" + message);
+    msg.send(message, done);
   }
 };
 
-var clearProjectCache = function(msg, endpoint, project) {
-  return msg.http(endpoint + "/project/" + project + "/build-cache?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
+var clearProjectCache = function(msg, endpoint, project, done) {
+  msg.http(endpoint + "/project/" + project + "/build-cache?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
     "Accept": "application/json"
   }).del('{}')(handleResponse(msg, function(response) {
-    return msg.send("Cleared build cache for " + project);
+    msg.send("Cleared build cache for " + project, done);
   }));
 };
 
-var clearAllProjectsCache = function(msg, endpoint) {
-  return msg.http(endpoint + "/projects?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
-    "Accept": "application/json"
-  }).get()(handleResponse(msg, function(response) {
-    var i, len, project, projectname, results;
-    results = [];
-    for (i = 0, len = response.length; i < len; i++) {
-      project = response[i];
-      projectname = escape(toProject(project.reponame));
-      results.push(clearProjectCache(msg, endpoint, projectname));
-    }
-    return results;
-  }));
-};
-
-var handleResponse = function(msg, handler) {
+var handleResponse = function(msg, handler, done) {
   return function(err, res, body) {
     var response;
-    if (err != null) {
-      msg.send("Something went really wrong: " + err);
+    if (err !== null) {
+      msg.send("Something went really wrong: " + err, done);
     }
     switch (res.statusCode) {
       case 404:
         response = JSON.parse(body);
-        return msg.send("I couldn't find what you were looking for: " + response.message);
+        msg.send("I couldn't find what you were looking for: " + response.message, done);
+        break;
       case 401:
-        return msg.send('Not authorized. Did you set NESTOR_CIRCLECI_TOKEN correctly?');
+        msg.send('Not authorized. Did you set NESTOR_CIRCLECI_TOKEN correctly?', done);
+        break;
       case 500:
-        return msg.send('Yikes! I turned that circle into a square');
+        msg.send('Yikes! I turned that circle into a square', done);
+        break;
       case 200:
         response = JSON.parse(body);
-        return handler(response);
+        handler(response);
+        break;
       default:
-        return msg.send("Hmm.  I don't know how to process that CircleCI response: " + res.statusCode, body);
+        msg.send("Hmm.  I don't know how to process that CircleCI response: " + res.statusCode + ": " +  body, done);
     }
   };
 };
@@ -145,7 +121,7 @@ module.exports = function(robot) {
         currentBuild = response[0];
         msg.send("Current status: " + (formatBuildStatus(currentBuild)), done);
       }
-    }));
+    }, done));
   });
 
   robot.respond(/circle last (\S*)\s*(\S*)/i, function(msg, done) {
@@ -172,63 +148,49 @@ module.exports = function(robot) {
   });
 
   robot.respond(/circle retry (.*) (.*)/i, function(msg, done) {
-    var branch, build_num, project, status;
+    var project = escape(toProject(msg.match[1]));
+    var build_num = escape(msg.match[2]);
 
-    if (msg.match[1] === 'all') {
-      status = escape(msg.match[2]);
-      getProjectsByStatus(msg, endpoint, status, 'retry', done);
-    } else {
-      project = escape(toProject(msg.match[1]));
-    }
-
-    build_num = escape(msg.match[2]);
     if (build_num === 'last') {
-      branch = 'master';
-      return msg.http(endpoint + "/project/" + project + "/tree/" + branch + "?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
+      var branch = 'master';
+      robot.http(endpoint + "/project/" + project + "/tree/" + branch + "?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
         "Accept": "application/json"
       }).get()(handleResponse(msg, function(response) {
-        var last;
-        last = response[0];
+        var last = response[0];
         build_num = last.build_num;
-        return retryBuild(msg, endpoint, project, build_num);
+        retryBuild(robot, msg, endpoint, project, build_num, done);
       }));
     } else {
-      return retryBuild(msg, endpoint, project, build_num);
+      retryBuild(robot, msg, endpoint, project, build_num, done);
     }
   });
 
-  robot.respond(/circle list (.*)/i, function(msg) {
-    var status;
-    status = escape(msg.match[1]);
+  robot.respond(/circle list (.*)/i, function(msg, done) {
+    var status = escape(msg.match[1]);
     if (status !== 'failed' && status !== 'success') {
-      msg.send("Status can only be failed or success.");
+      msg.send("Status can only be failed or success.", done);
       return;
     }
-    return getProjectsByStatus(msg, endpoint, status, 'list');
+    getProjectsByStatus(robot, msg, endpoint, status, 'list', done);
   });
 
-  robot.respond(/circle cancel (.*) (.*)/i, function(msg) {
-    var build_num, project;
-    project = escape(toProject(msg.match[1]));
-    if (msg.match[2] == null) {
-      msg.send("I can't cancel without a build number");
-      return;
-    }
-    build_num = escape(msg.match[2]);
-    return msg.http(endpoint + "/project/" + project + "/" + build_num + "/cancel?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
-      "Accept": "application/json"
-    }).post('{}')(handleResponse(msg, function(response) {
-      return msg.send("Canceled build " + response.build_num + " for " + project + " [" + response.branch + "]");
-    }));
-  });
-
-  robot.respond(/circle clear (.*)/i, function(msg) {
-    var project;
-    if (msg.match[1] === 'all') {
-      return clearAllProjectsCache(msg, endpoint);
+  robot.respond(/circle cancel (.*) (.*)/i, function(msg, done) {
+    var project = escape(toProject(msg.match[1]));
+    if (msg.match[2] === null) {
+      msg.send("I can't cancel without a build number", done);
     } else {
-      project = escape(toProject(msg.match[1]));
-      return clearProjectCache(msg, endpoint, project);
+      var build_num = escape(msg.match[2]);
+
+      robot.http(endpoint + "/project/" + project + "/" + build_num + "/cancel?circle-token=" + process.env.NESTOR_CIRCLECI_TOKEN).headers({
+        "Accept": "application/json"
+      }).post('{}')(handleResponse(msg, function(response) {
+        msg.send("Canceled build " + response.build_num + " for " + project + " [" + response.branch + "]", done);
+      }));
     }
+  });
+
+  robot.respond(/circle clear (.*)/i, function(msg, done) {
+    var project = escape(toProject(msg.match[1]));
+    clearProjectCache(msg, endpoint, project, done);
   });
 };
